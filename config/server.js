b3a32d42af4478abdffa1b42ee911751eec1b12e
slezakt <info@tomasslezak.cz>
2022-01-19 16:12:03 +0100

    config

--- config/server.js
+++ config/server.js
@@ -9,9 +9,8 @@ module.exports = ({ env }) => ({
   vercel: {
     token: env("pxAFjH89LUYDxx1DpCGXlYeF"),
     projectId: env("prj_Z4QcstDffevQKhYnmhtaca58G3JR"),
-    teamId: env("slezakt"), // optional: if project owner is a team
     triggers: {
-      production: env("Doq4r4SskN"),
+      production: env("Xu8ocNA1oX"),
     },
   },
 });
