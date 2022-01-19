d5cc2e169b029c718d49d9eb48ddb95d67e5c3cc
slezakt <info@tomasslezak.cz>
2022-01-19 15:53:08 +0100

    vercel config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,17 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '6b441450ff0886ba335df68cdfb59cd5'),
+      secret: env("ADMIN_JWT_SECRET", "6b441450ff0886ba335df68cdfb59cd5"),
+    },
+  },
+  vercel: {
+    token: env("pxAFjH89LUYDxx1DpCGXlYeF"),
+    projectId: env("prj_Z4QcstDffevQKhYnmhtaca58G3JR"),
+    teamId: env("slezakt"), // optional: if project owner is a team
+    triggers: {
+      production: env("Doq4r4SskN"),
     },
   },
 });
